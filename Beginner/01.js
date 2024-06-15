4278468671ba6ffbe5be5cc75e42a488169df93b
abdelquoddouss <fadliabdelquoddouss.06@gmail.com>
2024-06-15 17:32:31 +0200

    chore: Add tasks and function to print "Hello, World!"

--- Beginner/01.js
+++ Beginner/01.js
@@ -1,2 +1,25 @@
 
+///01: 
+
+// Version de base
+console.log("Hello, World!");
+
+// Tâche supplémentaire 1 : Imprimez « Bonjour le monde ! » en majuscule
+console.log("Bonjour le monde !".toUpperCase());
+
+// Tâche supplémentaire 2 : Imprimez « Bonjour le monde ! » en utilisant la concaténation de chaînes
+let greeting = "Bonjour";
+let world = "le monde !";
+console.log(greeting + " " + world);
+
+// Tâche supplémentaire 3 : Stockez la chaîne « Hello, World ! » dans une variable, puis imprimez-la sur la console
+let message = "Hello, World!";
+console.log(message);
+
+// Tâche supplémentaire 4 : Créez une fonction qui imprime « Hello, World ! » et appelez-le depuis votre code JavaScript
+function printHelloWorld() {
+    console.log("Hello, World!");
+}
+
+printHelloWorld();
 
